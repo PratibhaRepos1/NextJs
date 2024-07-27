feaa35a5aa3b732063baf9e6cad723cc14780b92
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2024-07-27 19:49:15 +0700

    Sections 5: Data fetching

---
+++ content-manager-app/pages/api/resources.js
@@ -0,0 +1,10 @@
+
+
+export default async function(req, res) {
+const dataRes = await fetch('http://localhost:3001/api/resources');
+const data = await dataRes.json();
+
+res.send(data);
+
+    
+}
--- content-manager-app/pages/index.js
+++ content-manager-app/pages/index.js
@@ -3,27 +3,46 @@ import ResourceHightlight from "@/components/ResourceHighlight";
 import Newsletter from "@/components/Newsletter";
 import ResourceList from "@/components/ResourceList";
 import Footer from "@/components/Footer";
+import resources from "./api/resources";
+import { useEffect } from "react";
 
-import { resources } from "@/api/data";
 
-function Home() {
+function Home({resources}) {
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3001/api/resources')
+  // }, [])
+
   return(
    <>  
 
     <Layout >
     <ResourceHightlight
-    resources = {resources}
+    resources = {resources.slice(0,2)}
     />
 
     <Newsletter />
     <ResourceList
-    resources = {resources} />
+    resources = {resources.slice(2)} />
     
      <Footer />
      </Layout>
 
    </>
   )
+}
+
+
+export async function getServerSideProps () {
+  const resData = await fetch("http://localhost:3001/api/resources");
+  const data = await resData.json();
+
+  return {
+    props: {
+     resources: data
+    }
+  }
+
 }
 
 export default Home;
